5a3dc222ab19a06d72899d33297825b32decc0e2
bsmk9 <bsmk9@DESKTOP-ANKQNB0.3520.dhcp.asu.edu>
2019-10-07 16:05:45 -0700

    Initial AddingMachine

---
+++ cse360assign2/src/AddingMachine.java
@@ -0,0 +1,39 @@
+
+/*
+ *  Name: Surya Boddu
+ *  ASU email: smboddu@asu.edu
+ *  Course: CSE 360 
+ *  Assignment Description: This assignment helps us get familiar with git hub
+ *                         repository. This program does numeric operations and returns string.       
+ * */
+
+public class AddingMachine
+ {
+   private int total;
+   public AddingMachine ()
+   {
+   total = 0;  // not needed - included for clarity
+   }
+  public int getTotal ()
+  {
+	 return 0;
+  }
+  public void add (int value)
+   {
+	 
+  }
+  
+  public void subtract (int value) 
+  {
+	 
+  }
+  public String toString ()
+  {
+  return ""; 
+  }
+  public void clear() 
+  {
+
+  }
+  
+}
