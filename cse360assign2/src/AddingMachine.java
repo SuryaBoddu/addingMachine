f9743f840e42f8e6e5190d87fe8214ca67f1acab
bsmk9 <bsmk9@DESKTOP-ANKQNB0>
2019-10-09 21:55:21 -0700

    Assignment2

--- cse360assign2/src/AddingMachine.java
+++ cse360assign2/src/AddingMachine.java
@@ -3,37 +3,55 @@
  *  Name: Surya Boddu
  *  ASU email: smboddu@asu.edu
  *  Course: CSE 360 
+ *  Assignment#2
  *  Assignment Description: This assignment helps us get familiar with git hub
  *                         repository. This program does numeric operations and returns string.       
  * */
 
-public class AddingMachine
- {
-   private int total;
-   public AddingMachine ()
-   {
-   total = 0;  // not needed - included for clarity
-   }
-  public int getTotal ()
-  {
-	 return 0;
-  }
-  public void add (int value)
-   {
-	 
-  }
-  
-  public void subtract (int value) 
-  {
-	 
-  }
-  public String toString ()
-  {
-  return ""; 
-  }
-  public void clear() 
-  {
+/**
+ * This is class AddingMachine The methods in this class do Mathematic
+ * operations like add, subtract and print out the string in a step by step
+ * process.
+ * <p>
+ * 
+ * @param nil
+ */
+public class AddingMachine {
+	private int total;
+	private String transactionHistory;
 
-  }
-  
+	public AddingMachine() // a constructor AddingMachine
+	{
+		total = 0;
+		transactionHistory = "0";
+	}
+
+	public int getTotal() // getTotal method returns the current value
+	{
+		return total;
+	}
+
+	public void add(int value) // add method which adds value to the total
+	{
+		total += value;
+		transactionHistory = transactionHistory + " + " + value;
+	}
+
+	public void subtract(int value) // subtract method which subtract value from the total
+	{
+		total -= value;
+		transactionHistory = transactionHistory + " - " + value;
+	}
+
+	public String toString() // toString method return string transactionHistory
+	{
+		return transactionHistory;
+	}
+
+	public void clear() // clear method to reset data
+	{
+		transactionHistory = "0";
+		total = 0;
+	}
+
 }
